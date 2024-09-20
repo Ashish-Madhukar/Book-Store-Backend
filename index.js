79773b9ee9dd5e0d93fec3cdfbb795d6727f2f5a
ASHISH KUMAR <120551608+Ashish-Madhukar@users.noreply.github.com>
2024-09-21 01:02:13 +0530

    git commit "updated"

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
- 
-
 app.get('/', (req, res) => {
     console.log("Hello World");
     res.send("Hello World from the backend!");
 });
+app.use(cors());
+app.use(express.json());
+ 
+
+
 
 dotenv.config();
 
